0627538eae9c44bc4ec552de37788bd81d774e04
JesseAbram <jesseabramowitz@hotmail.com>
2020-10-29 14:14:11 +0100

    custom pallet tests

---
+++ test/customPallet.test.js
@@ -0,0 +1,10 @@
+describe('custom pallet', () => {
+    it('tests a custom pallet', async () => {
+        const somethingStored = await halva.polkadot.query.templateModule.something()
+        const newStorage = Number(somethingStored.toHuman()) + 1
+        const tx = await halva.polkadot.tx.templateModule.doSomething(newStorage)
+        await passes(tx, 'doSomething', alicePair);
+        const somethingStoredAfter = await halva.polkadot.query.templateModule.something()
+        assert.equal(somethingStoredAfter.toString(), newStorage.toString())
+    });
+});
--- test/load.test.js
+++ test/load.test.js
@@ -1,14 +1,13 @@
-const {getApi, getKeypair, getTokenDecimals} = require('../utils/polkadot')
+const {getApi, getKeypair} = require('../utils/polkadot')
 
 describe('Loaded transfers', () => {
     
     const amountToSend = "100000000000"
-    const reruns = 4000
+    const reruns = 100
 
     it(`Transfer should make ${reruns} transfers of ${amountToSend}`, async () => {
         const api = await getApi()
         const alice = await getKeypair('//Alice')
-        const tokenDecimals = getTokenDecimals(api)
         const nonce = await api.rpc.system.accountNextIndex(alice.address);
         const balanceOfCharlieBefore = await api.query.system.account(charliePair.address)
         let i = 0
@@ -21,10 +20,7 @@ describe('Loaded transfers', () => {
         }
         await passes(tx, 'Send', alicePair);
         const balanceOfCharlieAfter = await api.query.system.account(charliePair.address)
-        console.log(balanceOfCharlieAfter.toHuman())
         const calculatedBalanceOfCharlieAfter = (Number(balanceOfCharlieBefore.data.free.toString())) + (Number(amountToSend) * reruns)
-        console.log({balanceOfCharlieAfter: balanceOfCharlieAfter.data.free.toString(), calculatedBalanceOfCharlieAfter: calculatedBalanceOfCharlieAfter.toString(), tokenDecimals})
         assert.equal(balanceOfCharlieAfter.data.free.toString(), calculatedBalanceOfCharlieAfter.toString(), "Charlie should have the proper balance")
     })
-
 })
