1aacf4157cbd46c41b3876a60a3d4f504ebc211f
JesseAbram <jesseabramowitz@hotmail.com>
2020-10-30 11:17:16 +0100

    load testing custom pallet

--- test/load.test.js
+++ test/load.test.js
@@ -1,13 +1,15 @@
 const {getApi, getKeypair} = require('../utils/polkadot')
 
-describe('Loaded transfers', () => {
+describe('Loaded transfers', async () => {
     
     const amountToSend = "100000000000"
-    const reruns = 100
+    const reruns = 1000
+    let alice 
+    let api
 
     it(`Transfer should make ${reruns} transfers of ${amountToSend}`, async () => {
-        const api = await getApi()
-        const alice = await getKeypair('//Alice')
+        api = await getApi()
+        alice = await getKeypair('//Alice')
         const nonce = await api.rpc.system.accountNextIndex(alice.address);
         const balanceOfCharlieBefore = await api.query.system.account(charliePair.address)
         let i = 0
@@ -22,5 +24,26 @@ describe('Loaded transfers', () => {
         const balanceOfCharlieAfter = await api.query.system.account(charliePair.address)
         const calculatedBalanceOfCharlieAfter = (Number(balanceOfCharlieBefore.data.free.toString())) + (Number(amountToSend) * reruns)
         assert.equal(balanceOfCharlieAfter.data.free.toString(), calculatedBalanceOfCharlieAfter.toString(), "Charlie should have the proper balance")
+    })
+
+    it(`templatePallet should make ${reruns} txs`, async () => {
+        // api = await getApi()
+        // alice = await getKeypair('//Alice')
+        const nonce = await api.rpc.system.accountNextIndex(alice.address);
+        let i = 0
+        let tx
+        while (i < reruns) {
+            const nextNonce = Number(nonce) + i 
+            tx = await api.tx.templateModule.doSomething(nextNonce).sign(alice, {nonce: nextNonce})
+            await tx.send()
+            i++
+        }
+        await passes(tx, 'doSomething', alicePair);
+        const nonceAfter = await api.rpc.system.accountNextIndex(alice.address);
+        const calculatedNonce = Number(nonce) + i
+        const calculatedSomethingAfter = Number(nonce) + reruns - 1
+        const somethingAfter = await api.query.templateModule.something()
+        assert.equal(somethingAfter.toString(), calculatedSomethingAfter.toString(), "something should have been incremented correctly")
+        assert.equal(nonceAfter.toString(), calculatedNonce.toString(), "all transactions should have fired")
     })
 })
