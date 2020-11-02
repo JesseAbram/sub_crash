284d0c035ca79b28cf2a26c7e0d60a817833f917
JesseAbram <jesseabramowitz@hotmail.com>
2020-11-02 15:07:51 +0100

    small change

--- test/customPallet.test.js
+++ test/customPallet.test.js
@@ -1,7 +1,7 @@
 describe('custom pallet', () => {
     it('tests a custom pallet', async () => {
         const somethingStored = await halva.polkadot.query.templateModule.something()
-        const newStorage = Number(somethingStored.toHuman()) + 1
+        const newStorage = Number(somethingStored.toString()) + 1
         const tx = await halva.polkadot.tx.templateModule.doSomething(newStorage)
         await passes(tx, 'doSomething', alicePair);
         const somethingStoredAfter = await halva.polkadot.query.templateModule.something()
